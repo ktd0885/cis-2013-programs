375731251767fe17fdf4cedd35085227f7eb5a3f
ktd0885 <54723323+ktd0885@users.noreply.github.com>
2019-09-23 13:48:55 -0500

    Add files via upload

---
+++ loan.js
@@ -0,0 +1,16 @@
+var $ = function (id) { return document.getElementById(id); }
+var calculate = function()
+{
+ var floatmonthlypayment, intloanterm, floattotalcost;
+ floatmonthlypayment = parseFloat ($("monthly_payment").value);
+ intloanterm = parseInt ($("loan_length").value);
+ floattotalcost = parseFloat (floatmonthlypayment + intloanterm);
+ $("total_cost").value = floattotalcost;
+ }
+ window.onload = function () 
+{
+    $("loan_length").value = "";
+    $("total_cost").value = "";
+    $("calc").onclick = calculate;
+    $("monthly_payment").focus();
+};
