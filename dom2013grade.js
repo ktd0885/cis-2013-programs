b74fc590f0d342c89fb26f6529873896fbcc55be
ktd0885 <54723323+ktd0885@users.noreply.github.com>
2019-10-14 14:34:04 -0500

    Add files via upload

--- dom2013grade.js
+++ dom2013grade.js
@@ -5,11 +5,23 @@ var $ = function (id)
 
 var calculate_click = function () 
 {
-    var floatHwPts, floatMidtermPts, floatFinalPts;
+    var floatHwPts, floatMidPts, floatFinPts, intGradeOption, stringFinalGrade
 
     floatHwPts = parseFloat($("hw_pts").value);
-    floatMidtermPts = parseFloat ($("Midterm_Pts").value);
-    floatFinalPts = parseFloat ($("Final_Pts").value);
+    floatMidtermPts = parseFloat ($("Mid_Pts").value);
+    floatFinalPts = parseFloat ($("Fin_Pts").value);
+    intGradeOption = parseInt ($("Grade_Option").value);
+
+(intGradeOption===1)
+{
+    if (floatTotalPts >= 80)
+    {
+      stringFinalGrade = "Pass";
+    }
+       else
+       {
+        stringFinalGrade ="Fail";
+       
 }
 
 window.onload = function () 
@@ -17,6 +29,6 @@ window.onload = function ()
     $("final_grade").value = stringFinalGrade; //blanks the final grade text box upon page load
     $("calculate").onclick = calculate_click; //activates main method when the button is clicked 
     $("hw_pts").focus(); //puts the cursor on the first DOM text input box
-    $("Midterm_Pts").value = "";
-    $("Final_Pts").value = "";
+    $("Mid_Pts").value = "";
+    $("Fin_Pts").value = "";
 };      
