1b6fa532d63f16bd51245dad5b56a0dfa2101953
ktd0885 <54723323+ktd0885@users.noreply.github.com>
2019-10-14 14:50:30 -0500

    Add files via upload

---
+++ gradeprototype.js
@@ -0,0 +1,34 @@
+var $ = function (id) 
+{
+    return document.getElementById(id);
+}
+
+var calculate_click = function () 
+{
+    var floatHwPts, floatMidPts, floatFinPts, intGradeOption, stringFinalGrade;
+
+    floatHwPts = parseFloat($("hw_pts").value);
+    floatMidPts = parseFloat ($("Mid_Pts").value);
+    floatFinPts = parseFloat ($("Fin_Pts").value);
+    intGradeOption = parseInt ($("1_or_2").value);
+
+(intGradeOption===1)
+{
+    if (floatTotalPts >= 80)
+    {
+      stringFinalGrade = "Pass";
+    }
+       else
+       {
+        stringFinalGrade ="Fail";
+       
+}
+
+window.onload = function () 
+{
+    $("final_grade").value = stringFinalGrade; //blanks the final grade text box upon page load
+    $("calculate").onclick = calculate_click; //activates main method when the button is clicked 
+    $("hw_pts").focus(); //puts the cursor on the first DOM text input box
+    $("Mid_Pts").value = "";
+    $("Fin_Pts").value = "";
+};
