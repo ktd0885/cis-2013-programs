c5e6963f21b10de355bd81737a091b089dd1e4a0
ktd0885 <54723323+ktd0885@users.noreply.github.com>
2019-10-14 14:15:03 -0500

    Add files via upload

--- dom2013grade.js
+++ dom2013grade.js
@@ -5,16 +5,16 @@ var $ = function (id)
 
 var calculate_click = function () 
 {
-    var floatHwPts;
+    var floatHwPts, floatMidtermPts, floatFinalPts
 
     floatHwPts = parseFloat($("hw_pts").value);
-
-
-};
+    floatMidtermPts = parseFloat ($("Midterm_Pts").value);
+    floatFinalPts = parseFloat ($("Final_Pts").value);
+}
 
 window.onload = function () 
 {
     $("final_grade").value = stringFinalGrade; //blanks the final grade text box upon page load
     $("calculate").onclick = calculate_click; //activates main method when the button is clicked 
     $("hw_pts").focus(id) = hw_pts; //puts the cursor on the first DOM text input box
-       
+};      
