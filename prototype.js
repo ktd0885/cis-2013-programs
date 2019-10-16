d6161e43147fe1b26439f9b88b8b91287b7d5ba8
ktd0885 <54723323+ktd0885@users.noreply.github.com>
2019-10-16 15:26:09 -0500

    Add files via upload

--- prototype.js
+++ prototype.js
@@ -10,21 +10,54 @@ var calculate_click = function ()
     floatHwPts = parseFloat($("hw_pts").value);
     floatMidPts = parseFloat ($("mid_pts").value);
     floatFinPts = parseFloat ($("fin_pts").value);
-    intGradeOption = parseInt ($("grade_option).value);
+    intGradeOption = parseInt ($("grade_option").value);
    floatTotalPts = parseFloat (floatHwPts + floatMidPts + floatFinPts);
-    $("final_grade").value) = floatTotalPts;
 
-(intGradeOption===1)
+if (intGradeOption===1)
 {
     if (floatTotalPts >= 80)
     {
-      stringFinalGrade = "Pass";
+            stringFinalGrade = "Pass";
     }
-       else
-       {
-        stringFinalGrade ="Fail";
-       
-} $("final_grade").value = stringFinalGrade;
+    else
+    {
+            stringFinalGrade ="Fail";
+    }
+}    
+else
+{
+   if (floatTotalPts >=90)
+   {
+         stringFinalGrade = "A";
+   }
+   else
+   {  
+         if(floatTotalPts>=80&&floatTotalPts<90)
+         {
+               stringFinalGrade = "B";
+         }
+         else
+         {
+               if (floatTotalPts >=70 && floatTotalPts <80)
+               {
+                  stringFinalGrade = "C";
+               }
+               else
+               {
+                  if (floatTotalPts >=60&&floatTotalPts<70)
+                  {
+                     stringFinalGrade = "D";
+                  }
+                  else
+                  {
+                     stringFinalGrade = "F";
+                  }
+               }   
+         }
+   }
+}
+   $("final_grade").value = stringFinalGrade;
+};
 
 window.onload = function () 
 {
