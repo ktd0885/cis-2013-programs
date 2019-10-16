8863200bb43ce719e7b9bd5f47a356db85a5669b
ktd0885 <54723323+ktd0885@users.noreply.github.com>
2019-10-16 15:27:32 -0500

    Add files via upload

--- gradeprototype.js
+++ gradeprototype.js
@@ -1,34 +1,68 @@
 var $ = function (id) 
 {
     return document.getElementById(id);
-}
+};
 
 var calculate_click = function () 
 {
-    var floatHwPts, floatMidPts, floatFinPts, intGradeOption, stringFinalGrade;
+    var floatHwPts, floatMidPts, floatFinPts, intGradeOption, floatTotalPts;
 
     floatHwPts = parseFloat($("hw_pts").value);
-    floatMidPts = parseFloat ($("Mid_Pts").value);
-    floatFinPts = parseFloat ($("Fin_Pts").value);
-    intGradeOption = parseInt ($("1_or_2").value);
+    floatMidPts = parseFloat ($("mid_pts").value);
+    floatFinPts = parseFloat ($("fin_pts").value);
+    intGradeOption = parseInt ($("grade_option").value);
+   floatTotalPts = parseFloat (floatHwPts + floatMidPts + floatFinPts);
 
-(intGradeOption===1)
+if (intGradeOption===1)
 {
     if (floatTotalPts >= 80)
     {
-      stringFinalGrade = "Pass";
+            stringFinalGrade = "Pass";
     }
-       else
-       {
-        stringFinalGrade ="Fail";
-       
+    else
+    {
+            stringFinalGrade ="Fail";
+    }
+}    
+else
+{
+   if (floatTotalPts >=90)
+   {
+         stringFinalGrade = "A";
+   }
+   else
+   {  
+         if(floatTotalPts>=80&&floatTotalPts<90)
+         {
+               stringFinalGrade = "B";
+         }
+         else
+         {
+               if (floatTotalPts >=70 && floatTotalPts <80)
+               {
+                  stringFinalGrade = "C";
+               }
+               else
+               {
+                  if (floatTotalPts >=60&&floatTotalPts<70)
+                  {
+                     stringFinalGrade = "D";
+                  }
+                  else
+                  {
+                     stringFinalGrade = "F";
+                  }
+               }   
+         }
+   }
 }
+   $("final_grade").value = stringFinalGrade;
+};
 
 window.onload = function () 
 {
-    $("final_grade").value = stringFinalGrade; //blanks the final grade text box upon page load
     $("calculate").onclick = calculate_click; //activates main method when the button is clicked 
     $("hw_pts").focus(); //puts the cursor on the first DOM text input box
-    $("Mid_Pts").value = "";
-    $("Fin_Pts").value = "";
+    $("mid_pts").value = "";
+    $("fin_pts").value = "";
 };
